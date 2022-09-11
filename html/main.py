fd522b62a9aca590a5712aad4a0a7f088dbce1f4
qazpl9 <tanronghao@yahoo.com.sg>
2022-09-11 20:10:22 +0800

    Initial Setup update 2

--- html/main.py
+++ html/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+app = Flask(__name__)
 
-app=Flask(__name__)
 @app.route("/")
-def home():
-    return "<h1>Nginx & Gunicorn</h1>"
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
