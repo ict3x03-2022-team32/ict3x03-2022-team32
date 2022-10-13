6867f09f7a69040321de00d1d7b89895924d9e3f
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2022-10-13 15:35:08 +0800

    Upload Skeleton Base App
    Upload Skeleton base app

---
+++ application/form.py
@@ -0,0 +1,94 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, IntegerField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, DataRequired, ValidationError
+from application.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
+
+class Form(FlaskForm):
+    # state = SelectField('state', choices=[('CA','California'), ('NV', 'Nevada')])
+    uname = SelectField('university',choices=[])
+    degName = SelectField('degName', choices=[])
+    industry = SelectField('industry',choices=[])
+    year = SelectField('year',choices=[])
+    colour = SelectField('colour',choices=[])
+
+
+class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
+    
+
+class UserDataForm(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                                choices=[('income', 'income'),
+                                        ('expense', 'expense')])
+    category = SelectField("Category", validators=[DataRequired()],
+                                            choices =[('rent', 'rent'),
+                                            ('salary', 'salary'),
+                                            ('investment', 'investment'),
+                                            ('side_hustle', 'side_hustle')
+                                            ]
+                            )
+    amount = IntegerField('Amount', validators = [DataRequired()])                                   
+    submit = SubmitField('Generate Report')
+
+
+class EmploymentDataForm(FlaskForm):
+    year = SelectField('Year', validators=[DataRequired()],
+                                choices=['2015', '2016',
+                                        '2017', '2018','2019'])
+    schoolName = StringField(label='School Name:', validators=[Length(min=2, max=60), DataRequired()])
+    degName = StringField(label='Degree Name:', validators=[Length(min=2, max=60), DataRequired()])
+    employmentRate = IntegerField('Employment Rate', validators = [DataRequired()])
+    salary = IntegerField('Salary', validators = [DataRequired()])
+    industry = SelectField("Industry", validators=[DataRequired()],
+                                            choices =['ICT', 'Healthcare',
+                                            'Engineering', 'Business',
+                                            'Arts'
+                                            ]
+                            )
+    submit = SubmitField('Generate Data')
+
+
+class IndustryDataForm(FlaskForm):
+    industryName = SelectField("Industry", validators=[DataRequired()],
+                                            choices =['ICT', 'Healthcare',
+                                            'Engineering', 'Business',
+                                            'Arts'
+                                            ]
+                            )
+    vacancy = IntegerField('Vacancy', validators = [DataRequired()])
+    year = SelectField('Year', validators=[DataRequired()],choices=['2015', '2016','2017', '2018','2019'])
+    submit = SubmitField('Generate Data')
+
+class EnrolmentDataForm(FlaskForm):
+    industryName = SelectField("Industry", validators=[DataRequired()],
+                                            choices =['ICT', 'Healthcare',
+                                            'Engineering', 'Business',
+                                            'Arts'
+                                            ]
+                            )
+    
+    year = SelectField('Year', validators=[DataRequired()],choices=['2015', '2016','2017', '2018','2019'])
+    intake = IntegerField('Intake', validators = [DataRequired()])
+    enrolment = IntegerField('Enrolment', validators = [DataRequired()])
+    graduates = IntegerField('Graduates', validators = [DataRequired()])
+    submit = SubmitField('Generate Data')                                         
---
+++ application/models.py
@@ -0,0 +1,107 @@
+from application import db, login_manager
+from application import bcrypt
+from datetime import datetime
+import enum
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Degree(db.Model):
+    degId = db.Column(db.Integer(), primary_key=True)
+    degName = db.Column(db.String(length=60), nullable=False, unique=True)
+
+class University(db.Model):
+    uId = db.Column(db.Integer(), primary_key=True)
+    uname = db.Column(db.String(length=60), nullable=False, unique=True)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
+
+
+class IncomeExpenses(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(30), default = 'income', nullable=False)
+    category = db.Column(db.String(30), nullable=False, default='rent')
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    amount = db.Column(db.Integer, nullable=False)
+    
+
+
+class employment(db.Model):
+    
+    geid = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, nullable=False)
+    schoolName = db.Column(db.String(60), nullable=False)
+    degName = db.Column(db.String(250), nullable=False)
+    employmentRate = db.Column(db.Float, nullable=False)
+    salary = db.Column(db.Integer, nullable=False)
+    industry = db.Column(db.String(60), nullable=False)
+
+    def __init__(self, year, schoolName, degName, employmentRate, salary, industry):
+ 
+        self.year = year
+        self.schoolName = schoolName
+        self.degName = degName
+        self.employmentRate = employmentRate
+        self.salary = salary
+        self.industry = industry
+
+
+class industry(db.Model):
+    
+    industryId = db.Column(db.Integer, primary_key=True)
+    industryName = db.Column(db.String(60), nullable=False)
+    vacancy = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, industryName, vacancy, year):
+ 
+        self.industryName = industryName
+        self.vacancy = vacancy
+        self.year = year
+
+
+class unienrolment(db.Model):
+    
+    uniId = db.Column(db.Integer, primary_key=True)
+    industry = db.Column(db.String(60), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    intake = db.Column(db.Integer, nullable=False)
+    enrolment = db.Column(db.Integer, nullable=False)
+    graduates = db.Column(db.Integer, nullable=False)
+    
+    def __init__(self, uniId, industry, year,intake,enrolment,graduates):
+ 
+        self.uniId = uniId
+        self.industry = industry
+        self.year = year
+        self.intake = intake
+        self.enrolment = enrolment
+        self.graduates = graduates
+
+
+
+
